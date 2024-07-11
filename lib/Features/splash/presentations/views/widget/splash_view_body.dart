6885b2bcb1e5373749ee7bbea01e76eb5dff5cca
mahmoud <87919952+Mahmoud14799@users.noreply.github.com>
2024-07-11 22:04:08 +0300

    create sliding animation for splash view

--- lib/Features/splash/presentations/views/widget/splash_view_body.dart
+++ lib/Features/splash/presentations/views/widget/splash_view_body.dart
@@ -1,8 +1,37 @@
 import 'package:flutter/material.dart';
+import 'package:pro_book/Features/splash/presentations/views/widget/sliding_animation.dart';
 import 'package:pro_book/core/utils/assets.dart';
 
-class SplashViewBody extends StatelessWidget {
+class SplashViewBody extends StatefulWidget {
   const SplashViewBody({super.key});
+
+  @override
+  State<SplashViewBody> createState() => _SplashViewBodyState();
+}
+
+class _SplashViewBodyState extends State<SplashViewBody>
+    with SingleTickerProviderStateMixin {
+  late AnimationController animationController;
+  late Animation<Offset> slidingAnimation;
+  late Animation<Offset> slidingAnimationImage;
+  @override
+  void initState() {
+    super.initState();
+    animationController = AnimationController(
+      vsync: this,
+      duration: const Duration(seconds: 2),
+    );
+    slidingAnimation = Tween<Offset>(
+      begin: const Offset(0, 10),
+      end: Offset.zero,
+    ).animate(animationController);
+    slidingAnimationImage = Tween<Offset>(
+      begin: const Offset(0, -10),
+      end: Offset.zero,
+    ).animate(animationController);
+
+    animationController.forward();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -10,13 +39,13 @@ class SplashViewBody extends StatelessWidget {
       mainAxisAlignment: MainAxisAlignment.center,
       crossAxisAlignment: CrossAxisAlignment.stretch,
       children: [
-        Image.asset(AssetsData.logo),
+        SlidingAnimation(slidingAnimation: slidingAnimationImage, child: Image.asset(AssetsData.logo)),
         const SizedBox(
           height: 4,
         ),
-        const Text(
-          'Read free book',
-          textAlign: TextAlign.center,
+        SlidingAnimation(
+          slidingAnimation: slidingAnimation,
+          child: const Text('Read Free Books',textAlign: TextAlign.center,),
         ),
       ],
     );
