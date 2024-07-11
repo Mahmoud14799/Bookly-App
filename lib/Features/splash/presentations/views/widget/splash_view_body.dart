36d21cdfd43b5458454275e1ca8f787f119912a3
mahmoud <87919952+Mahmoud14799@users.noreply.github.com>
2024-07-11 20:47:05 +0300

    add text to splash view

--- lib/Features/splash/presentations/views/widget/splash_view_body.dart
+++ lib/Features/splash/presentations/views/widget/splash_view_body.dart
@@ -7,11 +7,18 @@ class SplashViewBody extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Column(
-    mainAxisAlignment: MainAxisAlignment.center,
-    crossAxisAlignment: CrossAxisAlignment.stretch,
-  children: [
-    Image.asset(AssetsData.logo),
-  ],
+      mainAxisAlignment: MainAxisAlignment.center,
+      crossAxisAlignment: CrossAxisAlignment.stretch,
+      children: [
+        Image.asset(AssetsData.logo),
+        const SizedBox(
+          height: 4,
+        ),
+        const Text(
+          'Read free book',
+          textAlign: TextAlign.center,
+        ),
+      ],
     );
   }
 }
