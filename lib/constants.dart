ab2b7f83e23b0c341b14ff65e8060f78dd37e9f4
mahmoud <87919952+Mahmoud14799@users.noreply.github.com>
2024-07-13 23:37:25 +0300

    Add kPadding30 constant and remove unnecessary line in constants.dart file

--- lib/constants.dart
+++ lib/constants.dart
@@ -2,6 +2,8 @@ import 'package:flutter/material.dart';
 
 const kPrimaryColor = Color(0xff100B20);
 const kPrimaryDuration = Duration(milliseconds: 250);
+const kPadding30 = EdgeInsets.symmetric(horizontal: 30);
+
 const kGTSectraFine = 'GT Sectra Fine';
 const kMontserrat = 'Montserrat';
 const kMontserratSemiBold = 'MontserratBold';
