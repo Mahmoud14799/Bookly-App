c93281774229a01fce4e687d5604cbe3276ced81
mahmoud <87919952+Mahmoud14799@users.noreply.github.com>
2024-07-13 23:38:05 +0300

    Refactor home view body widget
    - Refactored the imports to match the new file structure
    - Added a nested scroll view with a sliver app bar and a sliver persistent header
    - Moved the best seller list to the body of the nested scroll view
    - Created a custom sliver persistent header delegate for the "Best Seller" section

--- lib/Features/Home/presentation/views/widget/home_view_body.dart
+++ lib/Features/Home/presentation/views/widget/home_view_body.dart
@@ -1,8 +1,8 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/widgets.dart';
-import 'package:pro_book/Features/Home/presentation/views/widget/best_seller_list.dart';
-import 'package:pro_book/Features/Home/presentation/views/widget/custom_app_bar.dart';
+import 'package:pro_book/Features/Home/presentation/views/widget/best_seller_list/best_seller_list.dart';
+import 'package:pro_book/Features/Home/presentation/views/widget/appBar/custom_app_bar.dart';
 import 'package:pro_book/Features/Home/presentation/views/widget/featuerd_list_view.dart';
+import 'package:pro_book/constants.dart';
 import 'package:pro_book/core/utils/style.dart';
 
 class HomeViewBody extends StatelessWidget {
@@ -10,28 +10,79 @@ class HomeViewBody extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Padding(
-      padding: EdgeInsets.only(left: 24),
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-          CustomAppBar(),
-          FeaturedBooksListView(),
-          SizedBox(
-            height: 51,
+    return NestedScrollView(
+      floatHeaderSlivers: true,
+      headerSliverBuilder: (BuildContext context, bool innerBoxIsScrolled) {
+        return <Widget>[
+          const SliverAppBar(
+          backgroundColor: kPrimaryColor,
+            automaticallyImplyLeading: false,
+            title: CustomAppBar(),
+            pinned: true,
+            floating: true,
           ),
-          Text(
-            'Best Seller',
-            style: Styles.textStyle18,
+          const SliverToBoxAdapter(
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                FeaturedBooksListView(),
+                SizedBox(
+                  height: 51,
+                ),
+              ],
+            ),
           ),
-          SizedBox(
-            height: 21,
+          SliverPersistentHeader(
+            pinned: true,
+            delegate: _SliverAppBarDelegate(
+              minHeight: 50.0,
+              maxHeight: 50.0,
+              child: Container(
+              padding: kPadding30,
+              alignment: Alignment.centerLeft,
+                color: kPrimaryColor,
+                child: const Text(
+                  'Best Seller',
+                  style: Styles.textStyle18,
+                
+                ),
+              ),
+            ),
           ),
-          Expanded(child: BestSellerList())
-        ],
-      ),
+        ];
+      },
+      body: const BestSellerList(),
     );
   }
 }
 
+class _SliverAppBarDelegate extends SliverPersistentHeaderDelegate {
+  _SliverAppBarDelegate({
+    required this.minHeight,
+    required this.maxHeight,
+    required this.child,
+  });
 
+  final double minHeight;
+  final double maxHeight;
+  final Widget child;
+
+  @override
+  double get minExtent => minHeight;
+
+  @override
+  double get maxExtent => maxHeight;
+
+  @override
+  Widget build(
+      BuildContext context, double shrinkOffset, bool overlapsContent) {
+    return SizedBox.expand(child: child);
+  }
+
+  @override
+  bool shouldRebuild(covariant _SliverAppBarDelegate oldDelegate) {
+    return oldDelegate.minHeight != minHeight ||
+        oldDelegate.maxHeight != maxHeight ||
+        oldDelegate.child != child;
+  }
+}
