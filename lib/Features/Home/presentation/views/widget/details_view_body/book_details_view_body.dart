65bfe161b3e4857ab6d365955d3ae86cb6f79a9b
mahmoud <87919952+Mahmoud14799@users.noreply.github.com>
2024-07-21 12:06:46 +0300

    Refactor book details view body widget

--- lib/Features/Home/presentation/views/widget/details_view_body/book_details_view_body.dart
+++ lib/Features/Home/presentation/views/widget/details_view_body/book_details_view_body.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:pro_book/core/utils/style.dart';
+import 'package:pro_book/core/widget/custom_buttom.dart';
 import 'package:pro_book/features/Home/presentation/views/widget/appBar/app_bar_details_view.dart';
 import 'package:pro_book/features/Home/presentation/views/widget/best_seller_list/book_rating_item.dart';
 import 'package:pro_book/features/Home/presentation/views/widget/features_list/custom_book_item.dart';
@@ -11,92 +12,54 @@ class BookDetailsViewBody extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     double hight = MediaQuery.of(context).size.height;
-    return Padding(
-      padding: EdgeInsets.symmetric(horizontal: 30.w),
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.center,
-        children: [
-          const AppBarDetailsView(),
-          SizedBox(height: 30.h),
-          Padding(
-            padding:
-                EdgeInsets.symmetric(horizontal: hight == 640.0 ? 100.w : 80.w),
-            child: const CustomBookImage(),
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.center,
+      children: [
+        const AppBarDetailsView(),
+        SizedBox(height: 30.h),
+        Padding(
+          padding:
+              EdgeInsets.symmetric(horizontal: hight >= 640.0 ? 125.w : 80.w),
+          child: const CustomBookImage(),
+        ),
+        SizedBox(height: 43.h),
+        Text(
+          "The Jungle Book",
+          style: Styles.textStyle30,
+        ),
+        Opacity(
+          opacity: 0.7,
+          child: Text(
+            'Rudyard Kipling',
+            style: Styles.textStyle18.copyWith(fontWeight: FontWeight.normal),
           ),
-          SizedBox(height: 43.h),
-          Text(
-            "The Jungle Book",
-            style: Styles.textStyle30,
+        ),
+        SizedBox(height: 15.h),
+        const BookRating(
+          mainAxisAlignment: MainAxisAlignment.center,
+        ),
+        SizedBox(
+          height: 37.h,
+        ),
+        const BookButtonAction(),
+        SizedBox(
+          height: hight == 640.0 ? 10 : 50,
+        ),
+        Padding(
+          padding: const EdgeInsets.only(right: 177),
+          child: Text(
+            'You can also like',
+            style: Styles.textStyle14.copyWith(fontWeight: FontWeight.w700),
           ),
-          Opacity(
-            opacity: 0.7,
-            child: Text(
-              'Rudyard Kipling',
-              style: Styles.textStyle18.copyWith(fontWeight: FontWeight.normal),
-            ),
-          ),
-          SizedBox(height: 15.h),
-          const BookRating(
-            mainAxisAlignment: MainAxisAlignment.center,
-          ),
-          SizedBox(
-            height: 37.h,
-          ),
-          Row(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              Container(
-                width: 130.w,
-                height: 48.h,
-                decoration: const BoxDecoration(
-                  color: Colors.white,
-                  borderRadius: BorderRadius.only(
-                    bottomLeft: Radius.circular(18),
-                    topLeft: Radius.circular(18),
-                  ),
-                ),
-                child: Center(
-                    child: Text(
-                  hight.toString(),
-                  style: Styles.textStyle18.copyWith(
-                      color: Colors.black, fontWeight: FontWeight.bold),
-                )),
-              ),
-              Container(
-                width: 130.w,
-                height: 48.h,
-                decoration: const BoxDecoration(
-                  color: Color(0xffEF8262),
-                  borderRadius: BorderRadius.only(
-                    bottomRight: Radius.circular(18),
-                    topRight: Radius.circular(18),
-                  ),
-                ),
-                child: Center(
-                    child: Text(
-                  'Free preview',
-                  style:
-                      Styles.textStyle16.copyWith(fontWeight: FontWeight.w700),
-                )),
-              )
-            ],
-          ),
-          SizedBox(
-            height: hight == 640.0 ? 10 : 50,
-          ),
-          Padding(
-            padding: const EdgeInsets.only(right: 177),
-            child: Text(
-              'You can also like',
-              style: Styles.textStyle14.copyWith(fontWeight: FontWeight.w700),
-            ),
-          ),
-          SizedBox(
-            height: 15.h,
-          ),
-          const LikeList(),
-        ],
-      ),
+        ),
+        SizedBox(
+          height: 15.h,
+        ),
+        const Padding(
+          padding: EdgeInsets.only(left: 30),
+          child: LikeList(),
+        ),
+      ],
     );
   }
 }
@@ -117,6 +80,33 @@ class LikeList extends StatelessWidget {
           );
         },
       ),
+    );
+  }
+}
+
+class BookButtonAction extends StatelessWidget {
+  const BookButtonAction({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Row(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        CustomButton(
+          text: '19.99€',
+          textColor: Colors.black,
+          backgroundColor: Colors.white,
+          borderRadius: BorderRadius.only(
+              topLeft: Radius.circular(12), bottomLeft: Radius.circular(12)),
+        ),
+        CustomButton(
+          text: 'Free preview',
+          textColor: Colors.white,
+          backgroundColor: Color(0xffEF8262),
+          borderRadius: BorderRadius.only(
+              topRight: Radius.circular(12), bottomRight: Radius.circular(12)),
+        )
+      ],
     );
   }
 }
