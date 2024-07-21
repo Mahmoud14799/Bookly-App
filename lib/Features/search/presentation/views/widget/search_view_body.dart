952ec5d90dbd19e4e20d5dfe9d7a69df20b20947
mahmoud <87919952+Mahmoud14799@users.noreply.github.com>
2024-07-21 18:39:44 +0300

    create search text field

--- lib/Features/search/presentation/views/widget/search_view_body.dart
+++ lib/Features/search/presentation/views/widget/search_view_body.dart
@@ -1,12 +1,51 @@
 import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
 class SearchViewBody extends StatelessWidget {
   const SearchViewBody({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Column(
-      children: [],
+    return const Padding(
+      padding: EdgeInsets.symmetric(horizontal: 30),
+      child: Column(
+        children: [
+          SizedBox(
+            height: 15,
+          ),
+          CustomSearchTextField(),
+        ],
+      ),
+    );
+  }
+}
+
+class CustomSearchTextField extends StatelessWidget {
+  const CustomSearchTextField({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return TextField(
+      decoration: InputDecoration(
+          focusedBorder: OutlineInputBorder(
+            borderRadius: BorderRadius.circular(10),
+            borderSide: const BorderSide(
+              color: Colors.amber,
+            ),
+          ),
+          enabledBorder: OutlineInputBorder(
+            borderRadius: BorderRadius.circular(10),
+            borderSide: const BorderSide(
+              color: Colors.grey,
+            ),
+          ),
+          hintText: 'Search',
+          suffixIcon: IconButton(
+              onPressed: () {},
+              icon: const Icon(
+                FontAwesomeIcons.magnifyingGlass,
+                size: 20,
+              ))),
     );
   }
 }
